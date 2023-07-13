2772b5747d310ba5dc90a6dbd1d51e5d23e9e4e4
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-07-13 17:59:32 -0400

    Agregar archivos de proyecto.

---
+++ MandezcaTest/Controllers/ClientController/ClientController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MandezcaTest.Controllers.ClientController
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientController
+    {
+
+    }
+}
