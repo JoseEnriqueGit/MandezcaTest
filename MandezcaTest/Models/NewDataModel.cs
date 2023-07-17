66c9145437c8ef482a574a49a2359eb61131652d
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-07-17 07:58:05 -0400

    New: model

---
+++ MandezcaTest/Models/NewDataModel.cs
@@ -0,0 +1,19 @@
+﻿namespace MandezcaTest.Models
+{
+    public class NewDataModel
+    {
+        public string ClientName { get; set; }
+        public string ClientEmail { get; set; }
+        public string ClientPhone { get; set; }
+
+        public string PerfilTitle { get; set; }
+        public string PerfilDescription { get; set; }
+
+        public string AddressLine { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+    }
+
+}
